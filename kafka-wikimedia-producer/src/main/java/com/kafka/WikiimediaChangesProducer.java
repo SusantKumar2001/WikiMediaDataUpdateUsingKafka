616ae907f8bcbb6f820ea3270e5a705153cdc356
susant <susanta1111baral@gmail.com>
2025-02-24 20:18:11 +0530

    Initial commit

---
+++ kafka-wikimedia-producer/src/main/java/com/kafka/WikiimediaChangesProducer.java
@@ -0,0 +1,37 @@
+package com.kafka;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+
+public class WikiimediaChangesProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikiimediaChangesProducer.class);
+
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public WikiimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() throws InterruptedException {
+        String topic = "wikimedia_recentchage";
+
+        //to read realtime stream data from wikimedia
+        EventHandler evenetHandler = new WikimediaChangesHandler(kafkaTemplate,topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(evenetHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
+        TimeUnit.MINUTES.sleep(10);
+
+    }
+}
